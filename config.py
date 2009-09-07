a5c44998ae3b46ce4b662414d56c7cc1d960b7cc
Cory Walker <cory@rusty-laptop.(none)>
2009-09-07 09:47:50 -0400

    still some tweaks to prevent lockups

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 IPOD_PATH = '/media/ipod'
-BUTTON_FORCE = 63
+BUTTON_FORCE = 60
 
 NOTE_WAIT = 3 #wait for note to be written
 BOOT_WAIT = 37.5 #Waiting for boot (my measurings said 33. Farthen)
 DISKMODE_WAIT = 12 #special one: FIXME
 REBOOT_COMBO_WAIT = 5.5
 DISKMODE_COMBO_WAIT = 10
-DISKMODE_DELAY = 10
+DISKMODE_DELAY = 12
 CRASH_THRESHOLD = 7
 
 LOG_FILENAME = 'nanotron.log'
@@ -19,3 +19,6 @@ IRC_PORT = 6667
 IRC_REALNAME = 'Nanotron-3000'
 IRC_PASSWORD = 'mypass'
 IRC_CHANNEL = '#linux4nano-nanotron'
+
+#merge diskmode_wait + diskmode_combo_wait
+#diskmode_delay should be flexible, not fixed
